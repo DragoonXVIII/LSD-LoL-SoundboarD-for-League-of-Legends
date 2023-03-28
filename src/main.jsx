4d1f89740163d25a15bc1f650ca9836211b13431
Kacper <81876955+februu@users.noreply.github.com>
2023-03-28 13:13:57 +0200

    install tauri
    - install tauri
    - create basic template for project
    - change file structure
    - modified App.jsx:
            - block right clicking in app window
            - add infinite request loop

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+import "./styles/styles.css";
+
+// Main DOM.
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
