af80ed02b2d6ccac535fd1e56d7e1cc5e9093ee5
Kacper <81876955+februu@users.noreply.github.com>
2023-06-29 20:43:54 +0200

    add goofy ass timer
    lmao
    this is next test if this shit will actually work XD

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
 import webview
-webview.create_window(
+import httpx
+import httpcore
+import time
+import json
+
+# playerName = "https://127.0.0.1:2999/liveclientdata/activeplayername"
+
+
+def runRequestLoop(window):
+    inGame = False
+    with httpx.Client(verify=False) as client:
+        while (True):
+            try:
+                request = client.get(
+                    "https://127.0.0.1:2999/liveclientdata/eventdata")
+                print(json.loads(request.text)['Events'])
+                if not inGame:
+                    inGame = True
+                    window.evaluate_js("window.runTimer();")
+            except Exception:
+                if inGame:
+                    inGame = False
+            time.sleep(0.5)
+
+
+window = webview.create_window(
     'LSD', 'webview/index.html', frameless=True)
-webview.start()
+webview.start(runRequestLoop, window)
 # debug=True
--- webview/index.js
+++ webview/index.js
@@ -1,14 +1,51 @@
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
+// function sleep(ms) {
+//   return new Promise((resolve) => setTimeout(resolve, ms));
+// }
 
-async function demo() {
-  while (true) {
-    var audio = new Audio("../assets/vine boom.mp3");
-    audio.volume = 0.3;
-    audio.play();
-    await sleep(5000);
+// async function demo() {
+//   while (true) {
+//     var audio = new Audio("../assets/vine boom.mp3");
+//     audio.volume = 0.3;
+//     audio.play();
+//     await sleep(5000);
+//   }
+// }
+
+// document.addEventListener("DOMContentLoaded", demo());
+
+let [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
+let timerRef = document.querySelector(".timerDisplay");
+let int = null;
+
+function displayTimer() {
+  milliseconds += 100;
+  if (milliseconds == 1000) {
+    milliseconds = 0;
+    seconds++;
+    if (seconds == 60) {
+      seconds = 0;
+      minutes++;
+      if (minutes == 60) {
+        minutes = 0;
+        hours++;
+      }
+    }
   }
+  let h = hours < 10 ? "0" + hours : hours;
+  let m = minutes < 10 ? "0" + minutes : minutes;
+  let s = seconds < 10 ? "0" + seconds : seconds;
+  let ms =
+    milliseconds < 10
+      ? "00" + milliseconds
+      : milliseconds < 100
+      ? "0" + milliseconds
+      : milliseconds;
+  timerRef.innerHTML = ` ${h} : ${m} : ${s} : ${ms}`;
 }
 
-document.addEventListener("DOMContentLoaded", demo());
+function runTimer() {
+  if (int !== null) {
+    clearInterval(int);
+  }
+  int = setInterval(displayTimer, 100);
+}
